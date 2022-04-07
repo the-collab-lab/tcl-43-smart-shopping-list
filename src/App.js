5ceba1c9fff5949678787bd3b7e4f6dc51f6fe61
nikkig <nikkig@nikkigs-MacBook-Pro.local>
2022-04-07 10:21:56 -0700

    Link  styling changes onClick

--- src/App.js
+++ src/App.js
@@ -1,32 +1,39 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 import { BrowserRouter, Routes, Link, Route } from 'react-router-dom';
 import List from './components/List.js';
 import AddItem from './components/AddItem.js';
 
 function App() {
-  const [btnClass, setBtnClass] = useState(false);
+  const handleClick = (e) => {
+    //e.target vs. e.currentTarget! e.currentTarget is what the event is attached to, e.target is what was clicked - fixed onClick confusion!
+
+    //why doesn't the path change on refresh, but the ui styling does??
+
+    const innerText = e.currentTarget.innerText;
+    if (innerText === 'Shopping List') {
+      e.currentTarget.style.fontWeight = 'bold';
+      e.currentTarget.nextSibling.style.fontWeight = 'normal';
+    } else {
+      e.currentTarget.style.fontWeight = 'bold';
+      e.currentTarget.previousElementSibling.style.fontWeight = 'normal';
+    }
+  };
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<List />} />
           <Route path="/additem" element={<AddItem />} />
-          <Route path="/list" element={<List />} />
         </Routes>
 
-        <div className="btn-div">
-          {/* how to remove the link styling */}
-          <button
-            onClick={() => {
-              btnClass ? setBtnClass(false) : setBtnClass(true);
-            }}
-            className={btnClass ? 'btnClass clicked' : 'btnClass'}
-          >
-            <Link to="/list">Shopping List</Link>
-          </button>
-          <button>
-            <Link to="/additem">Add Item</Link>
-          </button>
+        <div className="link-div">
+          <Link to="/" onClick={handleClick}>
+            Shopping List
+          </Link>
+          <Link to="/additem" onClick={handleClick}>
+            Add Item
+          </Link>
         </div>
       </BrowserRouter>
     </div>
