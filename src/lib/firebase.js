b68375cf18253b96953b3a32bf17056620ceaa44
Aditya Dalal <adidalal@users.noreply.github.com>
2022-04-10 20:44:18 -0400

    prettier reformat

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -4,12 +4,12 @@ import { getFirestore } from 'firebase/firestore';
 
 // Initialize Firebase.
 const firebaseConfig = {
-  apiKey: "AIzaSyDcrGMBbTzb2YouhAvovMKahVYktyLO6LY",
-  authDomain: "tcl-43-smart-shopping-list.firebaseapp.com",
-  projectId: "tcl-43-smart-shopping-list",
-  storageBucket: "tcl-43-smart-shopping-list.appspot.com",
-  messagingSenderId: "353075517348",
-  appId: "1:353075517348:web:f0c2953277ad5543261331"
+  apiKey: 'AIzaSyDcrGMBbTzb2YouhAvovMKahVYktyLO6LY',
+  authDomain: 'tcl-43-smart-shopping-list.firebaseapp.com',
+  projectId: 'tcl-43-smart-shopping-list',
+  storageBucket: 'tcl-43-smart-shopping-list.appspot.com',
+  messagingSenderId: '353075517348',
+  appId: '1:353075517348:web:f0c2953277ad5543261331',
 };
 
 export const app = initializeApp(firebaseConfig);
