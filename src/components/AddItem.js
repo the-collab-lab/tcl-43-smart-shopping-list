ecc5134a78ebbffb83dd69c54f1583513eddb29d
nikkig <nikkig@nikkigs-MacBook-Pro.local>
2022-04-12 18:33:28 -0700

    add item form

--- src/components/AddItem.js
+++ src/components/AddItem.js
@@ -1,16 +1,27 @@
 import { collection, addDoc } from 'firebase/firestore';
 import { db } from '../lib/firebase';
+import { useState } from 'react';
 
 export default function AddItem() {
-  const submitHandler = async () => {
+  const [itemName, setItemName] = useState('');
+  const [frequency, setFrequency] = useState(7);
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+
     try {
       const docRef = await addDoc(collection(db, 'Shopping-List'), {
-        item: 'Lettuce',
+        item: itemName,
+        frequency: frequency,
+        lastPurchaseDate: null,
       });
       console.log(docRef.id);
     } catch (e) {
       console.error(e);
     }
+
+    setItemName('');
+    setFrequency(7);
   };
 
   return (
@@ -18,24 +29,41 @@ export default function AddItem() {
       <h1>Add Item</h1>
       <form onSubmit={submitHandler}>
         <label htmlFor="item-name">Item name:</label>
-        <input required id="item-name" type="text" name="item-name" />
+        <input
+          required
+          id="item-name"
+          type="text"
+          name="item-name"
+          onChange={(e) => setItemName(e.target.value)}
+          value={itemName}
+        />
         <fieldset>
           <legend>How soon will you buy this again?</legend>
-
-          <label htmlFor="soon">Soon</label>
           <input
             defaultChecked
             id="soon"
             type="radio"
             name="frequency"
-            // RT: I think we will want to add a value and have that equal to the numerical value associated with each frequency, i.e. soon will have a value of 7. This will apply to each input element.
+            value={7}
+            onChange={(e) => setFrequency(e.target.value)}
           />
-
+          <label htmlFor="soon">Soon</label>
+          <input
+            id="kind-of-soon"
+            type="radio"
+            name="frequency"
+            value={14}
+            onChange={(e) => setFrequency(e.target.value)}
+          />
           <label htmlFor="kind-of-soon">Kind of Soon</label>
-          <input id="kind-of-soon" type="radio" name="frequency" />
-
+          <input
+            id="not-soon"
+            type="radio"
+            name="frequency"
+            value={30}
+            onChange={(e) => setFrequency(e.target.value)}
+          />
           <label htmlFor="not-soon">Not Soon</label>
-          <input id="not-soon" type="radio" name="frequency" />
         </fieldset>
         <button type="submit">Add Item</button>
       </form>
