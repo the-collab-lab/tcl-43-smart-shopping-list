5b874b9c13fcae9b76c399c814b94202ddbf6ad8
Rachael Thomas <rnthomas001@gmail.com>
2022-04-19 19:27:42 -0600

    Add notes for issue 6

--- src/components/AddItem.jsx
+++ src/components/AddItem.jsx
@@ -7,11 +7,30 @@ export default function AddItem() {
   const [userToken] = useState(getUser());
   const [itemName, setItemName] = useState('');
   const [frequency, setFrequency] = useState(7);
+  // Step 1:
+  // Add a state hook for message which has an initial value of an empty string and we can update that with the error message
 
   const submitHandler = async (e) => {
     e.preventDefault();
 
+    // NOTE: the user's original input should be what gets saved to the database, so we should not edit the setItemName logic
+
+    // Step 2:
+    // need to get the user's existing list from firestore
+    // I believe the code/logic is the same from the List.jsx file, lines 11 - 18 - if that is the case, maybe we move that into a helper function as well? Something we can look at to refactor after we get it to work
+
+    // Step 3:
+    // iterate (map?) over the list items and normalize the text (toLowerCase(), remove punctuation => we should think about having this normalization be a helper function in the storage-utils folder)
+    // assign the normalized list to a new variable
+    // idea for helper function:
+    // data.toLowerCase().replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g, "")
+
+    // Step 4:
+    // Use the helper function to normalize itemName and assign it to new variable
+
     try {
+      // Step 5:
+      // Add an if statement to check if the normalized list includes the normalized itemName - if it DOES (is true), update the message state from line 10 to be the error message to display - if it does NOT (is false), continue with the code below
       const docRef = await addDoc(collection(db, userToken), {
         item: itemName,
         frequency: frequency,
@@ -69,6 +88,9 @@ export default function AddItem() {
         </fieldset>
         <button type="submit">Add Item</button>
       </form>
+
+      {/* Step 6: */}
+      {/* add in conditional logic for message - if it's not an empty string, display the message */}
     </>
   );
 }
