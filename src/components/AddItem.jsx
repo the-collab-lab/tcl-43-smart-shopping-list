e8a7e999879147ae0bb5f0bece5d08e6223a0993
Lena Al <lena.al.developer@gmail.com>
2022-04-20 19:09:10 -0700

    first attempt adding the logic- still in progress

--- src/components/AddItem.jsx
+++ src/components/AddItem.jsx
@@ -1,48 +1,53 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { collection, addDoc } from 'firebase/firestore';
 import { db } from '../lib/firebase';
 import { getUser } from '../storage-utils/storage-utils';
+import { onSnapshot } from 'firebase/firestore';
 
 export default function AddItem() {
   const [userToken] = useState(getUser());
   const [itemName, setItemName] = useState('');
   const [frequency, setFrequency] = useState(7);
-  // Step 1:
-  // Add a state hook for message which has an initial value of an empty string and we can update that with the error message
+  const [message, setMessage] = useState('');
+  const [docs, setDocs] = useState([]);
+
+  useEffect(() => {
+    const unsubscribe = onSnapshot(collection(db, userToken), (snapshot) => {
+      let snapshotDocs = [];
+      snapshot.forEach((doc) => snapshotDocs.push(doc));
+      setDocs(snapshotDocs);
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, [userToken]);
 
   const submitHandler = async (e) => {
     e.preventDefault();
+    const regex = /[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g;
+    const cleanList = docs.map((listItem) =>
+      listItem.data().item.toLowerCase().replace(regex, ''),
+    );
 
-    // NOTE: the user's original input should be what gets saved to the database, so we should not edit the setItemName logic
-
-    // Step 2:
-    // need to get the user's existing list from firestore
-    // I believe the code/logic is the same from the List.jsx file, lines 11 - 18 - if that is the case, maybe we move that into a helper function as well? Something we can look at to refactor after we get it to work
-
-    // Step 3:
-    // iterate (map?) over the list items and normalize the text (toLowerCase(), remove punctuation => we should think about having this normalization be a helper function in the storage-utils folder)
-    // assign the normalized list to a new variable
-    // idea for helper function:
-    // data.toLowerCase().replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g, "")
-
-    // Step 4:
-    // Use the helper function to normalize itemName and assign it to new variable
-
+    const cleanItemName = itemName.toLowerCase().replace(regex, '');
     try {
-      // Step 5:
-      // Add an if statement to check if the normalized list includes the normalized itemName - if it DOES (is true), update the message state from line 10 to be the error message to display - if it does NOT (is false), continue with the code below
+      if (cleanList.includes(cleanItemName)) {
+        console.log('first');
+      }
       const docRef = await addDoc(collection(db, userToken), {
         item: itemName,
         frequency: frequency,
         lastPurchaseDate: null,
       });
       console.log(docRef.id);
+      setMessage(`${itemName} added to the list successfuly.`);
     } catch (e) {
+      setMessage(`${itemName} is already included in the list.`);
       console.error(e);
     }
-
     setItemName('');
     setFrequency(7);
+    console.log('second', message);
   };
 
   return (
