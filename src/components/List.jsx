115b9d702f53a76ba216951313c4b918e7ac57cd
simonkaine <simonkaine@gmail.com>
2022-04-26 13:46:36 -0700

    Added checkbox + Update purchased item successful, work in progress,

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,5 +1,11 @@
 import { useState, useEffect } from 'react';
-import { collection, onSnapshot } from 'firebase/firestore';
+import {
+  collection,
+  onSnapshot,
+  Timestamp,
+  doc,
+  updateDoc,
+} from 'firebase/firestore';
 import { db } from '../lib/firebase';
 import { getUser } from '../storage-utils/storage-utils';
 import Nav from './Nav';
@@ -7,6 +13,26 @@ import Nav from './Nav';
 export default function List() {
   const [docs, setDocs] = useState([]);
   const [userToken] = useState(getUser());
+
+  const handleCheckBox = (e, item) => {
+    e.preventDefault();
+    let now = Timestamp.now().seconds;
+    // console.log("NOW: ", now)
+    // let dayFromNow = now + 86400
+    // console.log("day later: ", dayFromNow);
+    const docItem = doc(db, userToken, item.id);
+    console.log('ITEM: ', docItem);
+
+    updateDoc(docItem, {
+      lastPurchaseDate: now,
+    });
+  };
+
+  const check24Hrs = (item) => {
+    //  have it check the last purchase date for the item
+    // take line 15 => run that difference.. eg; now - items purchase date?
+    console.log('check 24 item last purchase');
+  };
 
   useEffect(() => {
     const unsubscribe = onSnapshot(collection(db, userToken), (snapshot) => {
@@ -22,10 +48,22 @@ export default function List() {
   return (
     <>
       <h1>Shopping List</h1>
+
       <div>
-        {docs.map((item, index) => {
-          return <p key={index}>{item.data().item}</p>;
-        })}
+        <ul>
+          {docs.map((item, index) => {
+            return (
+              <li key={index}>
+                <input
+                  type="checkbox"
+                  onClick={(e) => handleCheckBox(e, item)}
+                  checked={check24Hrs(item)}
+                />
+                {item.data().item}
+              </li>
+            );
+          })}
+        </ul>
       </div>
 
       <Nav />
