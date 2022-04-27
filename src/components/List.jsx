45a697924f66eaf718378c11176bac630aa0ac06
nikkig <nikkig@nikkigs-MacBook-Pro.local>
2022-04-26 19:13:38 -0700

    clean up code, small refactor

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -3,14 +3,13 @@ import { collection, onSnapshot } from 'firebase/firestore';
 import { db } from '../lib/firebase';
 import { getUser } from '../storage-utils/storage-utils';
 import Nav from './Nav';
-import AddItem from './AddItem.jsx';
-import { NavLink } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 
 export default function List() {
   const [docs, setDocs] = useState([]);
   const [userToken] = useState(getUser());
   const navigate = useNavigate();
+
   useEffect(() => {
     const unsubscribe = onSnapshot(collection(db, userToken), (snapshot) => {
       let snapshotDocs = [];
