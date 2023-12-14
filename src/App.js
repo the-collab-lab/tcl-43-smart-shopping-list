bf50f23d1caa4a27aa100a9263668a8fe5b9961a
itsoliviasparks <itsoliviasparks@gmail.com>
2023-12-13 20:09:56 -0700

    feat: add ArchivalAnnouncementModal

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './components/Home/Home';
 import List from './components/List/List.jsx';
 import AddItem from './components/AddItem/AddItem.jsx';
 import { getUser } from './utils/utils';
+import { ArchivalNoticeModal } from '@the-collab-lab/shopping-list-utils';
 
 function App() {
   const userToken = getUser();
@@ -12,7 +13,23 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route exact path="/" element={userToken ? <List /> : <Home />} />
+          <Route
+            exact
+            path="/"
+            element={
+              userToken ? (
+                <>
+                  <ArchivalNoticeModal />
+                  <List />
+                </>
+              ) : (
+                <>
+                  <ArchivalNoticeModal />
+                  <Home />
+                </>
+              )
+            }
+          />
           <Route path="/list" element={<List />} />
           <Route path="/addItem" element={<AddItem />} />
         </Routes>
